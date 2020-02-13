8409556475881d7c8fb8c7f1a4ebf6fa8fcc623e
chriscohoat <chris.cohoat@gmail.com>
2020-02-13 11:46:01 -0500

    Initial commit with working code

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Container image that runs your code
-FROM alpine:3.10
+FROM cibuilds/aws:1.16.43
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+INSTANCE_IDS=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $2 | jq --raw-output '.AutoScalingGroups[0].Instances | map(.InstanceId) | join(";")')
+export IFS=";"
+for instance_id in $INSTANCE_IDS; do \
+    INSTANCE_IP_ADDRESS=$(aws ec2 describe-instances --instance-ids $instance_id | jq --raw-output '.Reservations[0].Instances[0].PublicIpAddress'); \
+    echo "Deploying to $INSTANCE_IP_ADDRESS..."; \
+    ssh -oStrictHostKeyChecking=no -i$3 ubuntu@"$INSTANCE_IP_ADDRESS" $1; \
+done
